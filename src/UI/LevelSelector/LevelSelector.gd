6440c06a496b29891f3b2268489d64c57cbae813
mahirlabibdihan <mahirlabibdihan@gmail.com>
2023-01-05 01:52:30 +0600

    Level progress added

--- src/UI/LevelSelector/LevelSelector.gd
+++ src/UI/LevelSelector/LevelSelector.gd
@@ -5,13 +5,20 @@ extends Control
 # var a = 2
 # var b = "text"
 
-
+onready var game_data = SaveProgress.game_data
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var backButton = get_node("LevelSelectorPanel/BackButton")
 	backButton.connect("pressed", self, "BackButtonClick")
 	
-
+	var i:int = 1;
+	for level_i in get_node("LevelSelectorPanel/LevelScroll/GridContainer").get_children():
+		level_i.get_node("Label").text = "Level " + str(i);
+		level_i.level_no = i
+		if(i > game_data.unlocked):
+			level_i.get_node("TextureRect").modulate.a = 0.2
+		i += 1
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
