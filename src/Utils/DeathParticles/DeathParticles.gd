19d4076f15538b6c06d93c6977410a0d2534675a
Abir Muhtasim <1905066@ugrad.cse.buet.ac.bd>
2023-01-05 12:42:23 +0600

    Player updated. deathparticles queue free added

---
+++ src/Utils/DeathParticles/DeathParticles.gd
@@ -0,0 +1,14 @@
+extends Particles2D
+
+
+onready var time_created = Time.get_ticks_msec()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Time.get_ticks_msec() - time_created > 50000:
+		queue_free()
