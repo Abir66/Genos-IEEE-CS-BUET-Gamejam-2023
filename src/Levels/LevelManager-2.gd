5b229ea36223da4b6f36b2ef602d95c12d507727
Abir Muhtasim <1905066@ugrad.cse.buet.ac.bd>
2023-01-07 20:56:38 +0600

    level manager added win lose condition, restart, resume,  exit signal

--- src/Levels/LevelManager-2.gd
+++ src/Levels/LevelManager-2.gd
@@ -4,7 +4,7 @@ var level : Node
 var level_no : int
 
 func _ready():
-	load_level(GameData.level_to_load)
+	load_level(2)
 	
 	
 func load_level(level_no):
@@ -16,11 +16,16 @@ func load_level(level_no):
 	
 	# Connect signals
 	level.connect("level_clear", self, "on_level_clear")
+	level.connect("level_lost", self, "on_level_lost")
 	level.connect("set_charge", self, "set_charge")
 	level.connect("set_health", self, "set_health")
 	
 	set_health(level.get_node("Player").get_health())
 	set_charge(level.get_node("Player").get_charge())
+	
+	GameData.level_lost = false
+	$InGameMenu/Pause.visible = false
+	
 	
 	
 func restart_level():
@@ -37,7 +42,10 @@ func on_level_clear():
 	next_level()
 
 func on_level_lost():
-	pass
+	GameData.level_lost = true
+	#pause and show game over menu
+	next_level()
+	
 	
 
 # Signals
@@ -46,3 +54,21 @@ func set_health(health_value):
 	
 func set_charge(charge_value):
 	$UI/ChargeBar.set_value(charge_value)
+
+
+func _on_PauseMenuPanel_exit_button():
+	get_tree().paused = false
+	$InGameMenu/Pause.visible = false
+	# need to add scene change to main menu
+	
+
+
+func _on_PauseMenuPanel_restart_button():
+	get_tree().paused = false
+	$InGameMenu/Pause.visible = false
+	restart_level()
+
+
+func _on_PauseMenuPanel_resume_button():
+	get_tree().paused = false
+	$InGameMenu/Pause.visible = false
