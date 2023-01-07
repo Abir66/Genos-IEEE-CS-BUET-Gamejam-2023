95c86ab48e9835e225927ed53063ddb9d47836a7
mahirlabibdihan <mahirlabibdihan@gmail.com>
2023-01-07 20:24:40 +0600

    Level unlock bug fix

--- src/UI/LevelSelector/LevelSelector.gd
+++ src/UI/LevelSelector/LevelSelector.gd
@@ -12,5 +12,17 @@ func _ready():
 			button.disabled = true
 		get_node("LevelSelectorPanel/GridContainer").add_child(button)
 
+func update_levels():
+	print(GameData.max_level_reached)
+	var i:int = 1;
+	for button in get_node("LevelSelectorPanel/GridContainer").get_children():
+		button.level_no = i
+		if(i <= GameData.max_level_reached):
+			button.get_node("TextureRect").modulate.a = 1
+			button.get_node("Label").modulate.a = 1
+			button.focus_mode = FOCUS_ALL
+			button.disabled = false
+		i += 1
+	
 func _on_BackButton_pressed() -> void:
 	self.visible = false
--- src/UI/MainMenu/MainMenu.gd
+++ src/UI/MainMenu/MainMenu.gd
@@ -9,6 +9,7 @@ func _ready():
 
 
 func _on_StartButton_pressed() -> void:
+	get_node("LevelSelector").update_levels()
 	get_node("LevelSelector").visible = true
 
 
