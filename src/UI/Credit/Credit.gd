d5cebfa2b913b93a7a01c7384087e114fd562508
mahirlabibdihan <mahirlabibdihan@gmail.com>
2023-01-07 19:19:14 +0600

    Signal connection simplified

--- src/UI/Credit/Credit.gd
+++ src/UI/Credit/Credit.gd
@@ -1,10 +1,4 @@
 extends Control
 
-
-func _ready():
-	var backButton = get_node("CreditPanel/BackButton")
-	backButton.connect("pressed", self, "BackButtonClick")
-
-func BackButtonClick():
-#	get_tree().change_scene("res://src/UI/MainMenu/MainMenu.tscn")
+func _on_BackButton_pressed() -> void:
 	self.visible = false
--- src/UI/MainMenu/MainMenu.gd
+++ src/UI/MainMenu/MainMenu.gd
@@ -3,27 +3,17 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var startButton = get_node("MainMenuPanel/StartButton")
-	var creditButton = get_node("MainMenuPanel/CreditsButton")
-	var exitButton = get_node("MainMenuPanel/ExitButton")
-	
-	startButton.connect("pressed", self, "StartButtonClick")
-	creditButton.connect("pressed", self, "CreditButtonClick")
-	exitButton.connect("pressed", self, "ExitButtonClick")
-	
 	get_node("LevelSelector").visible = false
 	get_node("Credits").visible = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
-func StartButtonClick():
+func _on_StartButton_pressed() -> void:
 	get_node("LevelSelector").visible = true
-#	get_tree().change_scene("res://src/UI/LevelSelector/LevelSelector.tscn")
 
-func ExitButtonClick():
-	get_tree().quit()
 
-func CreditButtonClick():
+func _on_CreditsButton_pressed() -> void:
 	get_node("Credits").visible = true
+
+
+func _on_ExitButton_pressed() -> void:
+	get_tree().quit()
