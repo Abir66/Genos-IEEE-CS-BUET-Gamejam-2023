6a418190f49aa60b9ab1a060dfb3820e3a130b71
mahirlabibdihan <mahirlabibdihan@gmail.com>
2023-01-07 18:30:03 +0600

    LevelSelector generated at run time

--- src/UI/LevelSelector/LevelButton.gd
+++ src/UI/LevelSelector/LevelButton.gd
@@ -1,15 +1,16 @@
-tool
+#tool
 extends Button
 
-export(String, FILE) var next_scene_path: = ""
+#export(String, FILE) var next_scene_path: = ""
 var level_no = 0
-onready var game_data = SaveProgress.game_data
 
 func _on_button_up() -> void:	
-	if game_data.unlocked >= level_no:
-		get_tree().change_scene(next_scene_path)
+	if GameData.max_level_reached  >= level_no:
+		GameData.level_to_load = level_no
+		print("level_to_load="+ String(level_no))
+		get_tree().change_scene("res://src/Levels/LevelManager-2.tscn")
 	else:
 		print(level_no)
-
-func _get_configuration_warning() -> String:
-	return "The property Next Level can't be empty" if next_scene_path == "" else ""
+#
+#func _get_configuration_warning() -> String:
+#	return "The property Next Level can't be empty" if next_scene_path == "" else ""
